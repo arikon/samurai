159045c26e86e31df4ca8cd01613e7016630b1ef
Sergey Kryzhanovsky <skryzhanovsky@ya.ru>
2011-10-26 17:19:28 +0400

    build initial

--- test/dir0/.samurai.js
+++ test/dir0/.samurai.js
@@ -3,7 +3,7 @@ exports.match = function(file) {
 };
 
 function resolve(file, tree, fsCache) {
-	var id = 'unit:' + file.self,
+	var id = 'unit+' + file.self,
 		node = tree.add(id, function() { console.log('building: ' + id) });
 	console.log('resolved: dir0');
 }
