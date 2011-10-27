cfe0ef900bd7eddef0f79a1ba021a5074f22bb92
Sergey Kryzhanovsky <skryzhanovsky@ya.ru>
2011-10-27 18:48:36 +0400

    better sample

--- test/sub/.samurai.js
+++ test/sub/.samurai.js
@@ -1,4 +1,5 @@
-var fs = require('fs');
+var fs = require('fs'),
+    path = require('path');
 
 exports.match = function(file) {
     if (file.self === 'trigger.txt') return resolve;
@@ -9,9 +10,22 @@ function resolve(file, tree, fsCache) {
         node = tree.add(id, build);
 
     node.custom['trigger'] = file.path;
+    node.custom['triggerOut'] = path.dirname(file.path) + '/sample.title.txt';
 }
 
 function build(node) {
-    var trigger = fs.readFileSync(node.custom['trigger']).toString();
-    console.log(trigger);
+    var trigger = fs.readFileSync(node.custom['trigger']).toString().trim(),
+        triggerOut = node.custom['triggerOut'],
+        old = path.existsSync(triggerOut) ? fs.readFileSync(triggerOut).toString().trim() : '',
+        title = old;
+
+    switch (trigger) {
+        case '1': title = 'Aleph'; break;
+        case '2': title = 'Beth'; break;
+        case '3': title = 'Gimel'; break;
+    }
+
+    if (title !== old) {
+        fs.writeFile(triggerOut, title + '\n');
+    }
 }
